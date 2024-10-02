873c8a327e824873ad96bd27544914aec8ac9d9d
Haikal Algivari <166151136+HaikalAlgivari@users.noreply.github.com>
2024-10-02 20:11:37 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,14 +17,6 @@ df_hour['dteday'] = pd.to_datetime(df_hour['dteday'])
 df_day.drop('instant', axis=1, inplace=True)
 df_hour.drop('instant', axis=1, inplace=True)
 
-# Change the 'season' column into object data type
-df_day['season'] = df_day['season'].map({1: 'Springer', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
-df_hour['season'] = df_hour['season'].map({1: 'Springer', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
-
-# Change the 'yr' column into object data type
-df_day['yr'] = df_day['yr'].map({0: 2011, 1: 2012})
-df_hour['yr'] = df_hour['yr'].map({0: 2011, 1: 2012})
-
 # Function to create the lineplot for mnth and cnt
 def create_lineplot_mnth(df, title):
   plt.figure(figsize=(10, 6))
@@ -128,6 +120,14 @@ selected_tab = st.sidebar.radio('Select Tab', ['Visualisasi & Analisis Deskripti
 
 # Visualisasi & Analisis Deskriptif
 if selected_tab == 'Visualisasi & Analisis Deskriptif':
+    # Change the 'season' column into object data type
+    df_day['season'] = df_day['season'].map({1: 'Springer', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
+    df_hour['season'] = df_hour['season'].map({1: 'Springer', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
+    
+    # Change the 'yr' column into object data type
+    df_day['yr'] = df_day['yr'].map({0: 2011, 1: 2012})
+    df_hour['yr'] = df_hour['yr'].map({0: 2011, 1: 2012})
+  
     st.header('Visualisasi & Analisis Deskriptif')
 
     st.subheader('Distribusi Jumlah Penyewaan Sepeda terhadap Waktu')
@@ -151,6 +151,14 @@ if selected_tab == 'Visualisasi & Analisis Deskriptif':
 
 # Analisis Lanjutan
 elif selected_tab == 'Analisis Lanjutan':
+    # Change the 'season' column into int data type
+    df_day['season'] = df_day['season'].map({'Springer': 1, 'Summer': 2, 'Fall': 3, 'Winter': 4})
+    df_hour['season'] = df_hour['season'].map({'Springer': 1, 'Summer': 2, 'Fall': 3, 'Winter': 4})
+
+    # Change the 'yr' column into int data type
+    df_day['yr'] = df_day['yr'].map({2011: 0, 2012: 1})
+    df_hour['yr'] = df_hour['yr'].map({2011: 0, 2012: 1})
+
     st.header('Analisis Lanjutan')
 
     st.subheader('Analisis Korelasi')
